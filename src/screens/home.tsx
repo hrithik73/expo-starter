8beebce19a16236e08214d4e02ff67d31aaf6fea
Hritik Singh <hrithiksinghvps_cse18@its.edu.in>
2023-12-15 13:09:41 +0530

    feat: added react-native-svg

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -1,9 +1,11 @@
 import { Text, View } from 'react-native';
+import HomeIcon from '~/assets/images/home.svg';
 
 const Home = () => {
   return (
     <View className='flex-1 justify-center items-center'>
       <Text>Hello</Text>
+      <HomeIcon />
     </View>
   );
 };
